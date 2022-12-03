970b5383a9ec366cf0f821012548aadadec36774
Karnikaa Velumani <karnikaavelumani@csu.fullerton.edu>
2022-12-02 23:15:02 -0800

    Complete Day 2 Challenge

---
+++ day2/day2part2.py
@@ -0,0 +1,29 @@
+with open('day2.txt') as text:
+    lines = [x.strip() for x in text.readlines()] # strip file (removes '\n')
+    # print(lines)
+
+    total = 0
+    # loop through each element
+    for val in lines:
+        if val == 'A X': # rock vs scissors (lose)
+            total += 3
+        if val == 'A Y': # rock vs rock (draw)
+            total += 4
+        if val == 'A Z': # rock vs paper (win)
+            total += 8
+
+        if val == 'B X': # paper vs scissors (win)
+            total += 1
+        if val == 'B Y': # paper vs rock (lose)
+            total += 5
+        if val == 'B Z': # paper vs paper (draw)
+            total += 9
+
+        if val == 'C X': # scissors vs scissors (draw)
+            total += 2
+        if val == 'C Y': # scissors vs rock (win)
+            total += 6
+        if val == 'C Z': # scissors vs paper (lose)
+            total += 7
+
+    print(total)
