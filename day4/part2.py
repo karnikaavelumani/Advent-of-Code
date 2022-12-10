5cb1429f403ff0edbfd324f29b7c47728a7f0cad
Karnikaa Velumani <karnikaavelumani@csu.fullerton.edu>
2022-12-10 10:19:59 -0800

    Challenge Day 4 Complete

---
+++ day4/part2.py
@@ -0,0 +1,22 @@
+def solution(lines):
+    count = 0
+    for line in lines:
+        sets = line.split(",")
+        sets1 = sets[0].split("-")
+        sets2 = sets[1].split("-")
+
+        start1 = int(sets1[0])
+        end1 = int(sets1[1])
+        start2 = int(sets2[0])
+        end2 = int(sets2[1])
+
+        if (((start1 <= start2) and (end1 >= end2)) or ((start1 >= start2) and (end1 <= end2))):
+            count += 1
+        elif (((start2 >= start1) and (start2 <= end1)) or ((end2 >= start1) and (end2 <= end1))):
+            count += 1
+
+    print(count)
+
+with open('input.txt') as text:
+    lines = text.read().split("\n")
+    solution(lines)
