2a111917336b233482beada3c9ea9e91fede6228
Karnikaa Velumani <karnikaavelumani@csu.fullerton.edu>
2023-12-01 15:59:39 -0800

    Complete Day 1 Part 2

--- aoc-2023/day1/part2.py
+++ aoc-2023/day1/part2.py
@@ -12,7 +12,7 @@ def solution(lines):
              'nine': 9 }
     add = 0
     for line in lines:
-        result = re.findall('one|^two|three|four|five|six|seven|eight|nine|[0-9]', line)
+        result = re.findall('(?=(one|two|three|four|five|six|seven|eight|nine|[1-9]))', line)
         if len(result) > 1:
             left = result[0]
             right = result[-1]
@@ -21,16 +21,18 @@ def solution(lines):
             if right in words:
                 right = str(words[right])
             t = left + right
-        else:
+        elif len(result) == 1:
             center = result[0]
             if center in words:
                 center = str(words[center])
             t = center + center
+        else:
+            print("Error")
         t_int = int(t)
         add += t_int
     print(add)
         
 
-with open('sample.txt') as text:
+with open('input.txt') as text:
     lines = text.read().split("\n")
     solution(lines)
