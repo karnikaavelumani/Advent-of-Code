b7819d4d3c667b017120c2d40d14734ae4e0c2ff
Karnikaa Velumani <karnikaavelumani@csu.fullerton.edu>
2023-11-30 21:44:18 -0800

    Complete day 1 part 1

--- aoc-2023/day1/part1.py
+++ aoc-2023/day1/part1.py
@@ -1,6 +1,15 @@
 def solution(lines):
+    arr = []
+    t = 0
     for line in lines:
-        print(line)
+        arr = list(line)
+        for x in arr[:]:
+            if x.isalpha():
+                arr.remove(x)
+        if len(arr) >= 1:
+            c = int(arr[0] + arr[-1])
+            t += c
+    print(t)
 
 with open('input.txt') as text:
     lines = text.read().split("\n")
