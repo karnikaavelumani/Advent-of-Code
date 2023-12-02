ae2dd4a2da119ddc6157db78b6f2d9186e7377d5
Karnikaa Velumani <karnikaavelumani@csu.fullerton.edu>
2023-12-01 22:44:24 -0800

    WIP

---
+++ aoc-2023/day2/part1.py
@@ -0,0 +1,21 @@
+def solution(lines):
+    colors = {"red": 12, "green": 13, "blue": 14}
+    print(lines)
+    
+
+with open('sample.txt') as text:
+    lines = {}
+    lines = text.read().strip()
+    # lines = lines.split(": ")
+    # game_id = lines[0].replace("Game ", "")
+    # sets = lines[1].split("; ")
+
+    
+    
+    solution(lines)
+
+# python3 main.py < input.dat
+
+# from sys import stdin
+# for line in stdin:
+#     line = line.strip()
