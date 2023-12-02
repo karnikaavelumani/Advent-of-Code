2fb19ff3dabb79864e18e4c5cfccf2cdaeb8390d
karnikaavelumani <60043611+karnikaavelumani@users.noreply.github.com>
2023-12-02 15:07:06 -0800

    WIP

--- aoc-2023/day2/part1.py
+++ aoc-2023/day2/part1.py
@@ -1,10 +1,30 @@
 def solution(lines):
     colors = {"red": 12, "green": 13, "blue": 14}
     for line in lines:
-        game_id = line.replace("Game ", "")
-        print(line)
-    
+        game_id = line[0].replace("Game ", "")
+        line = line[1].split("; ")
+        line = [x.split(", ") for x in line]
+        # print(line)
 
-with open('sample.txt') as text:
+        for l in line:
+            line = l.split(" ")
+
+        print(line)
+        # print(l)
+
+        # new_dict = {"id": game_id, "cubes": []}
+        # cube = {"red": [], "green": [], "blue": []}
+
+        # Create a frequency map for the given list
+        # freq_map = {}
+        # for l in line:
+        #     freq_map[l[0]] = freq_map.get(l[0], 0) + 1
+        # print(freq_map)
+
+
+with open("sample.txt") as text:
+    # Remove new line characters
     lines = [x.strip() for x in text.readlines()]
+    # Split lines by colon
+    lines = [x.split(": ") for x in lines]
     solution(lines)
